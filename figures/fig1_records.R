3936d18f15a4f0bc73efd183a5deab38b4309d43
DoriMG <15670245+DoriMG@users.noreply.github.com>
2026-01-16 11:29:08 +0100

    R figure code

---
+++ figures/fig1_records.R
@@ -0,0 +1,104 @@
+library(ggplot2)
+library(patchwork)
+library(forcats)
+library(dplyr)
+
+out_folder = "figs"
+ 
+data_file =  "data/merged_data.csv"
+df_ori <- read.csv(data_file, header=TRUE, stringsAsFactors=TRUE)
+
+df = df_ori[!duplicated(df_ori$Rayyan.ID), ]
+
+# Year published
+year = ggplot(df, aes(Year)) +
+  geom_histogram()+
+  labs(y ='Number of studies', x='Year of publication')+
+  theme_classic()
+year
+
+# Orders count
+orders = ggplot(df_ori, aes(x = fct_infreq(Order), fill=fct_infreq(Order))) +
+  geom_bar()+
+  labs(y ='Number of studies', x='Order', fill='Order')+
+  theme_classic()+theme(axis.text.x=element_text(angle = 45, vjust = 1.0, hjust=1))
+orders
+
+# Open Access
+open_access = ggplot(df, aes(x = fct_infreq(Open.access), fill=Open.access)) +
+  geom_bar()+
+  labs(y ='Count', x='Open Access', fill='Open Access')+
+  theme_classic()+ theme(legend.position="none")
+open_access
+
+table(df$Open.access)
+
+# Data freely available
+data_avail = ggplot(df, aes(x = fct_infreq(Data.availability), fill=fct_infreq(Data.availability))) +
+  geom_bar()+
+  labs(y ='Number of studies', x='Data availability', fill='Data availability')+
+  theme_classic()+ theme(legend.position="none")+theme(axis.text.x=element_text(angle = 45, vjust = 1.0, hjust=1))
+data_avail
+table(df$Data.availability)
+
+
+df$open_access = 0
+df[df$Open.access=='Yes',]$open_access = 1
+open_access_grouped = df %>%
+  group_by(Year) %>%
+  summarize(Mean = mean(open_access, na.rm=TRUE))
+open_access_grouped$Mean = open_access_grouped$Mean*100
+
+open_access_time = ggplot(open_access_grouped, aes(x = Year, y=Mean)) +
+  geom_point()+
+  labs(x ='Year', y='Open access papers (%)')+
+  theme_classic()
+open_access_time
+
+
+df$data_available = 1
+df[df$Data.availability=='No',]$data_available = 0
+data_grouped = df %>%
+  group_by(Year) %>%
+  summarize(Mean = mean(data_available, na.rm=TRUE))
+
+data_grouped$Mean = data_grouped$Mean*100
+data_avail_time = ggplot(data_grouped, aes(x = Year, y=Mean)) +
+  geom_point()+
+  labs(x ='Year', y='Data available (%)')+
+  theme_classic()
+data_avail_time
+
+fig1 = (year|orders)/
+  (open_access|data_avail)/
+  (open_access_time|data_avail_time)
+fig1
+
+ggsave(file.path(out_folder,'fig1_record_chars.pdf'),fig1, width = 8, height =10)
+ggsave(file.path(out_folder,'fig1_record_chars.png'),fig1, width = 8, height =10)
+
+
+##################################################################################
+## Supplementary
+## Species per order
+data_file =  "species_per_order.csv"
+df_spo <- read.csv(data_file, header=TRUE, stringsAsFactors=TRUE)
+order_order = levels(temp)
+species_per_order = ggplot(df_spo, aes(y=unique_species, x = factor(Order, order_order), fill=factor(Order, order_order))) +
+  geom_bar(stat="identity")+
+  labs(y ='Number of species', x='Order', fill='Order')+
+  theme_classic()+theme(axis.text.x=element_text(angle = 45, vjust = 0.8, hjust=1))
+species_per_order
+
+
+
+df['combine_journal'] = df['journal']
+levels(df$combine_journal) <- c(levels(df$combine_journal),"Other")
+dt = table(df$journal)
+single_journal = rownames(dt)[dt<3]
+df$combine_journal[df$journal %in% single_journal] <- 'Other'
+journal = ggplot(df, aes(x = fct_infreq(combine_journal), fill=fct_infreq(combine_journal))) +
+  geom_bar()+
+  labs(y ='Count', x='Journal', fill='Journal')+
+  theme_classic()+theme(axis.text.x=element_blank())
+journal
---
+++ figures/fig2_methods.R
@@ -0,0 +1,60 @@
+library(ggplot2)
+library(patchwork)
+library(forcats)
+
+out_folder = "figs"
+
+data_file =  "rec_locations.csv"
+df_loc <- read.csv(data_file, header=TRUE, stringsAsFactors=TRUE)
+df_loc$percs = df_loc$percs*100
+loc_order = c("field", 'lab', 'zoo', 'farm')
+recording_location = ggplot(df_loc, aes(y=percs, x = factor(location, loc_order) , fill=factor(location, loc_order)  )) +
+  geom_bar(stat="identity")+
+  labs(y ='Studies (%)', x='Location', fill='Location')+
+  theme_classic()+ theme(legend.position="none")
+recording_location
+
+data_file =  "contexts.csv"
+df_loc <- read.csv(data_file, header=TRUE, stringsAsFactors=TRUE)
+df_loc$percs = df_loc$percs*100
+loc_order = c("group", 'both', 'solitary')
+context = ggplot(df_loc, aes(y=percs, x = factor(context , loc_order) , fill=factor(context , loc_order)  )) +
+  geom_bar(stat="identity")+
+  labs(y ='Studies (%)', x='Context')+
+  theme_classic()+ theme(legend.position="none")
+context
+
+# Number of animals
+df_temp = df_ori[df_ori['n_animals']>0,]
+n_animals = ggplot(df_temp, aes(n_animals)) +
+  geom_histogram(bins = 20)+
+  scale_x_continuous(trans='log10')+
+  labs(y ='Count', x='Animals N')+
+  theme_classic()
+n_animals
+
+# Number of vocalizations
+df_temp = df_ori[df_ori['n_recs_analysed']>0,]
+n_recs_analysed = ggplot(df_temp, aes(n_recs_analysed)) +
+  geom_histogram(bins = 20)+
+  scale_x_continuous(trans='log10')+
+  labs(y ='Count', x='Vocalizations N')+
+  theme_classic()
+n_recs_analysed
+
+## Trends
+animals_over_time = ggplot(df_temp, aes(x = Year, y=n_animals)) +
+  geom_point()+
+  labs(x ='Year', y='Number of animals')+
+  theme_classic()+
+  scale_y_continuous(trans='log10')
+animals_over_time
+
+
+recs_over_time = ggplot(df_temp, aes(x = Year, y=n_recs_analysed)) +
+  geom_point()+
+  labs(x ='Year', y='Number of recordings')+
+  theme_classic()+
+  scale_y_continuous(trans='log10')
+recs_over_time
+
