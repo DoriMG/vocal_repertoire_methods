7cbcb1a6e651f2c460ab678da3781d5790aeac8d
DoriMG <15670245+DoriMG@users.noreply.github.com>
2026-02-01 12:00:02 +0100

    Add fig 2 R code

--- figures/fig2_methods.R
+++ figures/fig2_methods.R
@@ -4,7 +4,7 @@ library(forcats)
 
 out_folder = "figs"
 
-data_file =  "rec_locations.csv"
+data_file =  "data/rec_locations.csv"
 df_loc <- read.csv(data_file, header=TRUE, stringsAsFactors=TRUE)
 df_loc$percs = df_loc$percs*100
 loc_order = c("field", 'lab', 'zoo', 'farm')
@@ -14,17 +14,21 @@ recording_location = ggplot(df_loc, aes(y=percs, x = factor(location, loc_order)
   theme_classic()+ theme(legend.position="none")
 recording_location
 
-data_file =  "contexts.csv"
+data_file =  "data/contexts.csv"
 df_loc <- read.csv(data_file, header=TRUE, stringsAsFactors=TRUE)
 df_loc$percs = df_loc$percs*100
-loc_order = c("group", 'both', 'solitary')
-context = ggplot(df_loc, aes(y=percs, x = factor(context , loc_order) , fill=factor(context , loc_order)  )) +
+con_order = c("group", 'both', 'solitary')
+context = ggplot(df_loc, aes(y=percs, x = factor(context , con_order) , fill=factor(context , con_order)  )) +
   geom_bar(stat="identity")+
   labs(y ='Studies (%)', x='Context')+
   theme_classic()+ theme(legend.position="none")
 context
 
 # Number of animals
+
+data_file =  "data/merged_data.csv"
+df_ori <- read.csv(data_file, header=TRUE, stringsAsFactors=TRUE)
+
 df_temp = df_ori[df_ori['n_animals']>0,]
 n_animals = ggplot(df_temp, aes(n_animals)) +
   geom_histogram(bins = 20)+
@@ -57,4 +61,14 @@ recs_over_time = ggplot(df_temp, aes(x = Year, y=n_recs_analysed)) +
   theme_classic()+
   scale_y_continuous(trans='log10')
 recs_over_time
+
+
+all_figs =  (recording_location|context)/
+  (n_animals|n_recs_analysed)/
+  (animals_over_time|recs_over_time)
+all_figs
+
+
+ggsave(file.path(out_folder,'fig2_rec_methods.pdf'),all_figs, width = 8, height =10)
+ggsave(file.path(out_folder,'fig2_rec_methods.png'),all_figs, width = 8, height =10)
 
